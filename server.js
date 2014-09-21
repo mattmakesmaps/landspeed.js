b966304361cdc2494063df899025a56708839594
Matthew Kenny <matthewkenny@gmail.com>
2014-09-21 10:04:30 -0700

    added notes for server.js

--- server.js
+++ server.js
@@ -13,8 +13,15 @@ mapnik.register_default_fonts();
 mapnik.register_default_input_plugins();
 
 var stylesheet = process.argv[2];
+// the '+process.argv[3]' converts the parsed string to an integer.
 var port = +process.argv[3] || 8000;
+// parseInt() function used to convert from string to integer of given base.
+// In this case, specify base 10.
 var concurrency = parseInt(process.argv[4] || 32, 10);
+/**
+Ternary operation stating: if process.argv[5], then assign variable palette to a
+new instance of mapnik.Palette, or assign variable palette a value of false.
+**/
 var palette = process.argv[5] ? new mapnik.Palette(fs.readFileSync(process.argv[5]), 'act') : false;
 
 if (!stylesheet) {
@@ -29,14 +36,47 @@ var renderer = require('./renderer')({
 });
 
 function isPNG(data) {
+    /**
+     * The first 8 members of the data array are evaluated against these hex values.
+     * Each evaluation returns true or false.
+     *
+     * If any of the evaluations return false, the value of the entire && comparison
+     * is set to false, and the function returns false. However, if all evaluations
+     * return true, the value of the last evaluation (of data[7], which is true) is
+     * returned.
+     *
+     * Just an interesting example of logical operators in JS.
+     * See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_Operators
+     *
+     * I bet that this is checking the headers of a binary file to determine if
+     * the file is a PNG  or not.
+     */
     return data[0] === 0x89 && data[1] === 0x50 && data[2] === 0x4E &&
         data[3] === 0x47 && data[4] === 0x0D && data[5] === 0x0A &&
         data[6] === 0x1A && data[7] === 0x0A;
 }
 
+/**
+ * QUESTION: Why do we call the http.createServer() method
+ * rather then something like, new http.createServer().
+ * http.createServer() returns an object, aren't we essentially
+ * instanciating something?
+ *
+ * ANSWER(?): the http.createServer() method actually returns
+ * an instance of Server instanciated using the `new` keyword
+ * See: https://github.com/joyent/node/blob/master/lib/http.js#L61-L63
+ */
 var server = http.createServer(function(req, res) {
     var uri = url.parse(req.url.toLowerCase(), true);
 
+    /**
+     * uri.query is an object:
+     * { query param name: query param value }
+     *
+     * I think the anonymous function is a callback used
+     * to handle the return value from the renderer()
+     * method.
+     */
     renderer(uri.query, function(err, tile) {
         if (err || !tile || !isPNG(tile)) {
             res.writeHead(500, {
@@ -57,3 +97,10 @@ server.listen(port, function() {
     var address = server.address();
     console.warn('Listening at %s:%d', address.address, address.port);
 });
+
+/**
+ * TODO: Lookup pattern for setting up a node server using the http module.
+ * In this script, it's a 2-step process.
+ * 1. assign a variable to http.createServer() with a callback to handle request and response.
+ * 2. call listen() on variable assigned in step 1, with a port and callback function.
+**/
